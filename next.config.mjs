d622ff5f6620047f85f22bf379f31b62406fcdb3
lishulin <lishulin@haomo.ai>
2025-04-17 15:49:38 +0800

    perf: 优化打包配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
+const mode = "export";
+console.log("[Next] build mode", mode);
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 let userConfig = undefined
 try {
   // try to import ESM first
@@ -27,6 +42,9 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  basePath,
+  assetPrefix,
+  output: mode,
 }
 
 if (userConfig) {
